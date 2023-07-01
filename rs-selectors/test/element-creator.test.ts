a805dacdeca41d03e5360d72b127b8e48e87c4d0
Ахантьева Ирина Александровна <i.akhanteva@s7.ru>
2023-07-01 16:27:04 +0400

    feat: add tests for methods of ElementCreator

--- rs-selectors/test/element-creator.test.ts
+++ rs-selectors/test/element-creator.test.ts
@@ -1,14 +1,34 @@
 import ElementCreator from '../src/app/util/element-creator';
 
-describe('getSum', () => {
+describe('Check methods of ElementCreator', () => {
+  const elementParams = {
+    tag: 'div',
+    classNames: ['test'],
+    textContent: '',
+    callback: null,
+  };
+  let testElement: ElementCreator;
+  const innerElement = document.createElement('span');
+  beforeAll(() => {
+    testElement = new ElementCreator(elementParams);
+    testElement.setCssClasses(['testing-element', 'css-class']);
+    testElement.setTextContent('test string');
+    testElement.addInnerElement(innerElement);
+  });
+
   it('should return HTMLElement', () => {
-    expect(
-      new ElementCreator({
-        tag: 'div',
-        classNames: ['test'],
-        textContent: '',
-        callback: null,
-      }).getElement(),
-    ).toBeDefined();
+    expect(testElement.getElement()).toBeDefined();
+  });
+
+  it('should set CSS', () => {
+    expect(testElement.getElement().className).toEqual('test testing-element css-class');
+  });
+
+  it('should set textContent', () => {
+    expect(testElement.getElement().textContent).toEqual('test string');
+  });
+
+  it('should add inner element', () => {
+    expect(testElement.getElement().contains(innerElement)).toBeTruthy();
   });
 });
